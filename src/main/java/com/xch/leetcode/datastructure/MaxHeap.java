b46cc34972a90d80c1564f4286888d9d15c8d3a4
HobbyBear <1962494765@qq.com>
2019-07-29 08:22:55 +0800

    stack ,heap

---
+++ src/main/java/com/xch/leetcode/datastructure/MaxHeap.java
@@ -0,0 +1,10 @@
+package com.xch.leetcode.datastructure;
+
+/**
+ * 最大堆
+ * @author: xch
+ * @create: 2019-07-28 12:22
+ **/
+public class MaxHeap {
+
+}
--- src/main/java/com/xch/leetcode/datastructure/MyStack.java
+++ src/main/java/com/xch/leetcode/datastructure/MyStack.java
@@ -10,6 +10,9 @@ public class MyStack {
 
     private int[] arr;
 
+    /**
+     * 栈的容量
+     */
     private int capacity;
     /**
      * 栈的大小
@@ -29,8 +32,46 @@ public class MyStack {
         this.head = -1;
     }
 
+    public void put(int num) {
+        if (size == capacity) {
+            throw new RuntimeException("栈已满");
+        }
+        head = head + 1 == capacity ? 0 : head + 1;
+        arr[head] = num;
+        size++;
+    }
+
+    /**
+     * 出栈
+     *
+     * @return
+     */
+    public int offer() {
+        int res = arr[head];
+        head = head == 0 ? capacity - 1 : head - 1;
+        size--;
+        return res;
+    }
 
 
+    public int getSize() {
+        return size;
+    }
 
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public static void main(String[] args) {
+        MyStack myStack = new MyStack(3);
+        myStack.put(1);
+        myStack.put(2);
+        System.out.println("stack 的大小是:" + myStack.getSize());
+        int num = myStack.offer();
+        System.out.println("取出" + num + "后的大小是" + myStack.getSize());
+        myStack.put(3);
+        System.out.println(myStack.offer());
+        System.out.println(myStack.offer());
+    }
 
 }
