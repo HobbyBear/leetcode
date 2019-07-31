17787b09c2bcd9d234f4d0d3f8fd3f185d93e4f0
XiongChuanHong <18881934641@163.com>
2019-07-31 08:41:11 +0800

    p2 两数之和

--- src/main/java/com/xch/leetcode/suanfa/p2/Solution.java
+++ src/main/java/com/xch/leetcode/suanfa/p2/Solution.java
@@ -12,6 +12,28 @@ class ListNode {
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+        ListNode head = new ListNode(0);
+        helper(head, l1, l2, 0);
+        return head.next;
+    }
+
+
+    private void helper(ListNode preNode, ListNode l1, ListNode l2, int carry) {
+        if (l1 == null && l2 == null && carry == 0){
+            return;
+        }
+        int res = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
+        int unitNum = res;
+        if (res >= 10) {
+            carry = 1;
+            unitNum = Integer.valueOf(String.valueOf(res).substring(1));
+        }else {
+            carry = 0;
+        }
+        ListNode node = new ListNode(unitNum);
+        if (preNode != null) {
+            preNode.next = node;
+        }
+        helper(node, (l1 == null ? null : l1.next), (l2 == null ? null : l2.next), carry);
     }
 }
